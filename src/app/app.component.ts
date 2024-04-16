e80995612ee122730ca73affdaeccdc7a1ea7884
Emilien <emilien_77400@hotmail.fr>
2024-04-16 14:14:52 +0200

    commit ws function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,10 @@ import { HttpClient } from '@angular/common/http';
 // import { HttpClientModule } from '@angular/common/http'; // Importez HttpClientModule
 
 import { Component, OnInit } from '@angular/core';
-import {RouterOutlet} from "@angular/router";
-import {DecimalPipe, NgIf} from "@angular/common";
-import {HttpClientModule} from "@angular/common/http";
+import { RouterOutlet } from "@angular/router";
+import { DecimalPipe, NgIf } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { WeatherService } from './weather.service';
 
 @Component({
   selector: 'app-root',
@@ -26,36 +27,12 @@ export class AppComponent implements OnInit {
   userCoordinates: { latitude: number; longitude: number; } | undefined;
   weatherData: any; // Stocker les données météo
 
-  constructor(private http: HttpClient) {} // Injectez HttpClient ici
+  constructor(private http: HttpClient) { } // Injectez HttpClient ici
 
   ngOnInit() {
-    this.getUserLocation();
-  }
-
-  getUserLocation(): void {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        this.userCoordinates = {
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude
-        };
-        this.getWeatherData();
-      });
-    } else {
-      console.log("No support for geolocation");
-    }
-  }
-  getWeatherData(): void {
-    const apiKey = 'b050d0b64ae3ca445a124d163582ba38';
-    if (this.userCoordinates) {
-      const { latitude, longitude } = this.userCoordinates; // Récupérez latitude et longitude de userCoordinates
-      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=48.9579&lon=2.8794&appid=${apiKey}`;
-
-      this.http.get(apiUrl).subscribe((data) => {
-        this.weatherData = data;
-        console.log("Weather Data:", this.weatherData);
-      });
-    }
+    const ws = new WeatherService(this, this.http)
+    ws.getUserLocation();
   }
 }
+
 
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,17 +1,43 @@
 // weather.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { AppComponent } from './app.component';
 
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
 export class WeatherService {
-    constructor(private http: HttpClient) {}
+  user: AppComponent;
+  client: HttpClient;
 
-    getWeatherData(latitude: number, longitude: number) {
-        const apiKey = 'b050d0b64ae3ca445a124d163582ba38';
-        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+  constructor(userInfo: AppComponent, clientInfo: HttpClient) {
+    this.user = userInfo
+    this.client = clientInfo
+  }
 
-        return this.http.get(apiUrl);
+  getUserLocation(): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        this.user.userCoordinates = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        };
+        this.getWeatherData();
+      });
+    } else {
+      console.log("No support for geolocation");
     }
+  }
+  getWeatherData(): void {
+    const apiKey = 'b050d0b64ae3ca445a124d163582ba38';
+    if (this.user.userCoordinates) {
+      const { latitude, longitude } = this.user.userCoordinates; // Récupérez latitude et longitude de userCoordinates
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=48.9579&lon=2.8794&appid=${apiKey}&lang=fr`;
+
+      this.client.get(apiUrl).subscribe((data) => {
+        this.user.weatherData = data;
+        console.log("Weather Data:", this.user.weatherData);
+      });
+    }
+  }
 }
